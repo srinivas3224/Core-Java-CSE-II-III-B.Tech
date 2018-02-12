64655c3e60c98ef25ec96a0c81d275c2d1a0469a
Devulapalli Srinivas <srinivas3224@gmail.com>
2018-02-12 16:20:30 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+package com.mvr.cse23.corejava.ds;
+class A
+{
+}
+class B
+{
+}
+class C
+{
+}
+class D
+{
+}
+class Test
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This is test class");
+	}
+	void m1()
+	{
+	}
+}
